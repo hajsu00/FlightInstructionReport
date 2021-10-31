7bf6e350e51aa59b9e40330ba1588905ca626efc
unknown <hajsu00@gmail.com>
2021-10-31 20:22:02 +0900

    test of extract data from Kintone DB

--- createTrainingReport.js
+++ createTrainingReport.js
@@ -7,47 +7,19 @@
         element.id='get_space';
         element.innerText ='取得';
         element.onclick = function () {
+            //const rec = kintone.app.record.get();
 
-            //const trainee = '';
-            const rec = kintone.app.record.get();
-            /*
-            if (rec){
-                //var trainee = rec['record']['練習生']['value'][0].name;   //連想配列
-                trainee = rec.record.練習生.value;
-            }
-            */
-            //const trainee = rec.record['練習生'].value[0].name;
-            const trainee = rec.record.練習生.value;
-            if (trainee == null){
-                window.alert('練習生を選択してください。');
-            }else{
-                //作成日時を取得する
-                //const createDate = rec.record.作成日時.value[0].value;
-                //window.alert(createDate);
-                //getLogFromHakkoukiroku(trainee, draftingDate, subject);
-                window.alert(trainee);
-                /*
-                var params = {
-                    //'app' : kintone.app.getId(),
-                    app : kintone.app.getId(),    //https://developer.cybozu.io/hc/ja/articles/202331474-%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89%E3%81%AE%E5%8F%96%E5%BE%97-GET-
-                    fields: ['前席'],
-                    query: '練習生 in ("' + user + '")' order by 
-                };
-                //https://muac.cybozu.com/k/8/
-                //kintone.api(pathOrUrl, method, params, successCallback, failureCallback)
-                kintone.api(kintone.api.url('/k/8/record.json', true), 'GET', params, function(resp){
-                    //success:レコード番号を表示する
-                    alert(resp.record.record_no.value);
-                }, function(resp){
-                    // error:エラーの場合はメッセージを表示する
-                    var errmsg = 'レコード取得時にエラーが発生しました。';
-                    // レスポンスにエラーメッセージが含まれる場合はメッセージを表示する
-                    if(resp.message !== undefined){
-                        errmsg += '\n' + resp.message;
-                    }
-                    alert(errmsg);
-                */
-            };
+            //const trainee = rec.record.練習生.value[0].name;    //出力結果（フィールド値を設定/未設定）＝＞ '（正常に表示）'/ '（空欄）'
+            //const trainee = rec.record.練習生.value[0];         //出力結果（フィールド値を設定/未設定）＝＞ '[Object] [Object]'/'（undefind）'
+            const trainee = rec.record.練習生.value;              //出力結果 （フィールド値を設定/未設定）＝＞ '[Object] [Object]'/'（空欄）'
+            //const trainee = event["record"]["練習生"]["value"];
+            
+            var key = Object.keys(trainee);
+            //console.log( key );
+
+            //window.alert(key);
+            console.log(key);
+
         }
         kintone.app.record.getSpaceElement('get_space').appendChild(element);
     });
