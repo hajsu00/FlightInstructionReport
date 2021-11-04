85a5d9fc2f017830a7611df3f8effe166c96df94
unknown <hajsu00@gmail.com>
2021-11-04 11:12:16 +0900

    [hotfix]不要なコメントアウトを削除

--- createTrainingReport.js
+++ createTrainingReport.js
@@ -4,29 +4,19 @@
     kintone.events.on('app.record.create.show', function(event) {
         //ボタン
         const element = document.createElement('button');
-        element.id='get_space';
+        element.id='get_studentInfo';
         element.innerText ='取得';
         element.onclick = function () {
-            //const rec = kintone.app.record.get();
-
-            //const trainee = rec.record.練習生.value[0].name;    //出力結果（フィールド値を設定/未設定）＝＞ '（正常に表示）'/ '（空欄）'
-            //const trainee = rec.record.練習生.value[0];         //出力結果（フィールド値を設定/未設定）＝＞ '[Object] [Object]'/'（undefind）'
-            const trainee = rec.record.練習生.value;              //出力結果 （フィールド値を設定/未設定）＝＞ '[Object] [Object]'/'（空欄）'
-            //const trainee = event["record"]["練習生"]["value"];
-            
-            var key = Object.keys(trainee);
-            //console.log( key );
-
-            //window.alert(key);
-            console.log(key);
-
+            const rec = kintone.app.record.get();
+            const studentName = rec.record.練習生.value;
+            if (studentName.length === 0){
+                alert('練習生を選択してください');
+            }else{
+                console.log(studentName[0].name);
+                console.log('studentName = ', studentName);
+            }
         }
-        kintone.app.record.getSpaceElement('get_space').appendChild(element);
+        kintone.app.record.getSpaceElement('get_studentInfo').appendChild(element);
+        return event;
     });
 })();
-
-    /*
-    function getLogFromHakkoukiroku(a, b, c, d){
-
-    }
-    */
