bbd96f8d6bd2e2f87a6698c9ce30dd21898aa001
unknown <hajsu00@gmail.com>
2021-11-04 21:36:20 +0900

    ユーザー属性「操縦教官」での分岐の実装完了

--- createTrainingReport.js
+++ createTrainingReport.js
@@ -2,7 +2,23 @@
     "use strict";
 
     kintone.events.on('app.record.create.show', function(event) {
-        //ボタン
+
+        //ユーザーが操縦教官であるか判定する
+        kintone.api(        //ユーザーの所属組織エクスポート API（JSON）
+        kintone.api.url('/v1/user/organizations', true), // pathOrUrl
+        'GET', // method
+        {code:kintone.getLoginUser()['code']}, // params
+        function(resp) { // 成功時のcallback
+            //some()メソッドで指定の要素があるか判定する。あればtrueが返る。
+            if(resp.organizationTitles.some(element => element = '操縦教官')){      //https://techacademy.jp/magazine/22267
+                console.log('ユーザーは操縦教官です');
+            }else{
+                console.log('ユーザーは操縦教官ではありません');
+            }
+        }
+        );
+
+        //取得ボタン
         const element = document.createElement('button');
         element.id='get_studentInfo';
         element.innerText ='取得';
