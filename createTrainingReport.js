e52cd8a78de1d380aa10040207c0db42162cdcac
unknown <hajsu00@gmail.com>
2021-11-07 19:55:39 +0900

    最新のレコード番号取得機能実装

--- createTrainingReport.js
+++ createTrainingReport.js
@@ -5,24 +5,59 @@
         //フィールドを編集不可にする
         disableBasicFields(event);
 
+
         //取得ボタン
         const element = document.createElement('button');
         element.id='get_studentInfo';
         element.innerText ='取得';
         element.onclick = function () {
             const rec = kintone.app.record.get();
+
+            //過去のレコード有無を調べる
             const studentName = rec.record.練習生.value;
             if (studentName.length === 0){
                 alert('練習生を選択してください');
             }else{
-                //練習生の飛行経歴を取得する
+                //グループフィールドを見やすく開閉する
+                kintone.app.record.setGroupFieldOpen('練習生情報', true);
 
+                //練習生の飛行経歴を取得する
+                return new kintone.Promise(function(resolve, reject){       //https://tech.nerune.co/other/kintone-promise/
+                    const paramLatestId = {
+                        "app": 33,
+                        "query": "order by $id desc limit 1 offset 0",
+                    };
+                    const LatestId = getLatestId(paramLatestId, event);
+                    console.log(LatestId);
+/*
+                    kintone.api(
+                        kintone.api.url('/k/v1/records.json', true),
+                        'GET',
+                        paramLatestId,
+                        function(resp) {
+                            const recordId = resp.records[0].$id.value;
+                            console.log('latestId', recordId);
+                            //return recordId;
+                      }, function(resp) {
+                            const errmsg = 'レコード取得時にエラーが発生しました。';
+                            // レスポンスにエラーメッセージが含まれる場合はメッセージを表示する
+                            if (resp.message !== undefined) {
+                            errmsg += '\n' + resp.message;
+                        }
+                        alert(errmsg);
+                        resolve(event);
+                      }
+                    );
+                    */
+                });
+
+                console.log(studentName[0].name);
 
 
 
                 //練習生の取組みテーマを取得する
-
-
+                
+                
 
 
 
@@ -31,7 +66,7 @@
         kintone.app.record.getSpaceElement('get_studentInfo').appendChild(element);
 
         //ユーザーが操縦教官であるか判定する
-        return new kintone.Promise(function(resolve, reject){
+        return new kintone.Promise(function(resolve, reject){       //https://tech.nerune.co/other/kintone-promise/
             kintone.api(        //ユーザーの所属組織エクスポート API（JSON）
                 kintone.api.url('/v1/user/organizations', true), // pathOrUrl
                 'GET', // method
@@ -39,17 +74,41 @@
                 function(resp) { // 成功時のcallback
                     //some()メソッドで指定の要素があるか判定する。あればtrueが返る。
                     if(resp.organizationTitles.some(element => element = '操縦教官')){      //https://techacademy.jp/magazine/22267
-                        console.log('ユーザーは操縦教官です');
                         
-                    }else{
-                        console.log('ユーザーは操縦教官ではありません');
+                        
+                    }else{  //ログインユーザーが練習生の場合
+                        //評価（教官記入欄）を編集不可にする
                         disableEvaluateFields(event);
+                        //グループフィールドを見やすく開閉する
+                        kintone.app.record.setGroupFieldOpen('フライト振り返り', true);
+
                     }
                     resolve(event);
                 }
             );
         });
     });
+
+    function getLatestId(myParam, myEvent){
+        kintone.api(
+            kintone.api.url('/k/v1/records.json', true),
+            'GET',
+            myParam,
+            function(resp) {
+                const recordId = resp.records[0].$id.value;
+                console.log('latestId', recordId);
+                //return recordId;
+          }, function(resp) {
+                const errmsg = 'レコード取得時にエラーが発生しました。';
+                // レスポンスにエラーメッセージが含まれる場合はメッセージを表示する
+                if (resp.message !== undefined) {
+                errmsg += '\n' + resp.message;
+            }
+            alert(errmsg);
+            resolve(myEvent);
+          }
+        );
+    }
 
     function disableBasicFields(myEvent){
         myEvent.record.起票日.disabled = true;
